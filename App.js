e34e0a46e4d43635251388fffdcebd7475567e06
Shubham kumar <sk15024040@gmail.com>
2021-08-09 11:50:21 +0530

    Weather-App using React-Native

--- App.js
+++ App.js
@@ -1,12 +1,51 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-
+import { StatusBar } from "expo-status-bar";
+import React, { useEffect, useState } from "react";
+import { StyleSheet, Text, View, ImageBackground } from "react-native";
+import * as Location from "expo-location";
+import DateTime from "./components/DateTime";
+import FutureForecast from "./components/FutureForecast";
+import WeatherScroll from "./components/WeatherScroll";
+const API_KEY = "8a7c6bdb68db1da97f3a248259dcea95";
+const img = require("./assets/App-Background.jpg");
 export default function App() {
+  const [data, setData] = useState({});
+  useEffect(() => {
+    (async () => {
+      let { status } = await Location.requestForegroundPermissionsAsync();
+      if (status !== "granted") {
+        fetchDataFromApi("30.1759", "-78.1242");
+        return;
+      }
+
+      let location = await Location.getCurrentPositionAsync({});
+      fetchDataFromApi(location.coords.latitude, location.coords.longitude);
+    })();
+  }, []);
+
+  const fetchDataFromApi = (latitude, longitude) => {
+    if (latitude && longitude) {
+      fetch(
+        `https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&exclude=hourly,minutely&units=metric&appid=${API_KEY}`
+      )
+        .then((res) => res.json())
+        .then((data) => {
+          console.log(data);
+          setData(data);
+        });
+    }
+  };
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <ImageBackground source={img} style={styles.image}>
+        <DateTime
+          current={data.current}
+          timezone={data.timezone}
+          lat={data.lat}
+          lon={data.lon}
+        />
+        <WeatherScroll weatherData={data.daily} />
+      </ImageBackground>
     </View>
   );
 }
@@ -14,8 +53,6 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
+  image: { flex: 1, resizeMode: "cover", justifyContent: "center" },
 });
